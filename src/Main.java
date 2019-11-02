137524730179fe48558a1741dc52340a1b00f2c6
Franz Kohnle <franz@kohnlehome.de>
2019-11-02 21:14:48 +0100

    Add Threads

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Test");
+        Object lockA = new Object();
+        Object lockB = new Object();
+
+        Thread thread1 = new Thread(new FirstAThenB(lockA, lockB));
+        Thread thread2 = new Thread(new FirtsBThenA(lockA, lockB));
+        thread1.start();
+        thread2.start();
     }
 }
